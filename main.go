2a497e2386258f0c9e880b59da7c0312e5c935fc
Sameer <sameer.barha12@gmail.com>
2023-03-10 23:12:25 +0530

    successfully parsed urls

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import ("fmt"
 		"flag"
 		"os"
+		"bufio"
+		"net/url"
 )
 
 var path *string
+var sliceUrls []string
+
+func checkNilErr(err error){
+	if err != nil{
+		panic(err)
+	}
+}
 
 func init(){
 	fmt.Println("init")
@@ -23,5 +32,24 @@ func main(){
 
 	fmt.Println("path to file ", *path)
 	fmt.Println("main")
-	
+
+	//Read line by line
+	file, err := os.Open(*path)
+	checkNilErr(err)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan(){
+
+		target_url := scanner.Text()
+		_ , err := url.ParseRequestURI(target_url)
+		if(err!=nil){
+			fmt.Println("Skipping: ", target_url)
+			//fmt.Println(msg)
+		}else{
+			fmt.Println("OK: ", target_url)
+			sliceUrls = append(sliceUrls, target_url)
+		}
+	}
+	fmt.Println("Scanner end")
 }
