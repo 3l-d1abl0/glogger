a516920ba0d7e126019d48e7c499ca0e578c995e
Sameer <sameer.barha12@gmail.com>
2023-12-06 17:41:41 +0530

    added test to check invalid arguments

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -7,7 +7,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestArgs(t *testing.T) {
+/*
+Ideally a setUp and tearDown
+function should create a file and output folder
+
+*/
+
+func TestValidArgs(t *testing.T) {
 
 	filePath := "/media/murphy/P3/groundzero/glogger/urls/local-testing.txt"
 	outputFolder := "/media/murphy/P3/groundzero/glogger/output"
@@ -26,7 +32,37 @@ func TestArgs(t *testing.T) {
 	fileArg, folderArg, err := GetArgs()
 
 	//Should be true
-	//assert.Equal(t, cliStatus, true)
-
 	assert.True(t, err == nil && filePath == fileArg && outputFolder == folderArg, "All valid")
+}
+
+func TestInvalidArg(t *testing.T) {
+
+	var testArgs = []struct {
+		flag1      string
+		filePath   string
+		flag2      string
+		folderPath string
+		expected   *string
+		comments   string
+	}{
+		{"--file", "/media/murphy/P3/groundzero/glogger/urls/local-testing1.txt", "-o", "/media/murphy/P3/groundzero/glogger/output", nil, "File path Invalid"},
+		{"--file", "/media/murphy/P3/groundzero/glogger/urls/local-testing.txt", "--output", "/media/murphy/P3/groundzero/glogger/input", nil, "Folder Path Invalid"},
+		{"-f", "/media/murphy/P3/groundzero/glogger/urls/local-testing2.txt", "-o", "/media/murphy/P3/groundzero/glogger/input", nil, "Both input file and output folder does not exist"},
+	}
+
+	//Get the cli Context
+	rootCmd := cli.GetCli()
+
+	for _, test := range testArgs {
+
+		//Set the arguments for cli Context
+		rootCmd.SetArgs([]string{test.flag1, test.filePath, test.flag2, test.folderPath})
+
+		//parse the arguments
+		_, _, err := GetArgs()
+
+		//Should be true
+		assert.True(t, err != nil, err)
+	}
+
 }
